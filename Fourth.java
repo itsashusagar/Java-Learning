f9f07ad6adcfa7057d1ea8831bb9ee780bf3a84b
ashu sagar <itsashusagar@gmail.com>
2024-11-15 19:20:38 +0530

    commit

---
+++ Fourth.java
@@ -0,0 +1,13 @@
+import java.util.*;;
+
+public class Fourth {
+    public static void main(String args[]){
+           
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter Your First Name: ");    
+            String firstname = sc.nextLine();
+            System.out.println("Enter Your Last Name: ");
+            String lastname = sc.nextLine();
+            System.out.println("Your Name is: " + firstname + " " + lastname);
+    }
+}
