d7866f548eb68f4e5700ff332a30b8f44f8eb960
ashu sagar <itsashusagar@gmail.com>
2024-11-15 19:15:14 +0530

    commit

---
+++ Third.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Third {
+    public static void main(String args[]){
+
+        // Addition Question: 
+        
+         Scanner sc = new Scanner(System.in);
+            System.out.println("Enter Your First Number: ");
+            int num1 = sc.nextInt();
+            System.out.println("Enter Your Second Number: ");
+            int num2 = sc.nextInt();
+            int sum = num1+num2;
+            System.out.println("Total Sum is: "+sum);
+    }
+}
