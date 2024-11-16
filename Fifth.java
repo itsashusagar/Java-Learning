d55fbb2fc096c54abd28013cfc7cd620679df0c3
ashu sagar <itsashusagar@gmail.com>
2024-11-16 17:06:38 +0530

    commit

---
+++ Fifth.java
@@ -0,0 +1,11 @@
+public class Fifth{
+
+    public static void helloWorld(){
+        System.out.println("Helloooo Worlddddd........");
+        System.out.println("I Am Ashu Sagar....");
+    }
+    public static void main(String args[]){
+     helloWorld();
+     helloWorld();
+    }
+}
